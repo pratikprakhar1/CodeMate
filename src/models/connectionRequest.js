77422a78272428cd036bcc97ee2983456f6f0fcf
pratikprakhar <b421033@iiit-bh.ac.in>
2024-10-18 10:35:29 +0530

    Indexing Added

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -21,7 +21,7 @@ const connectionRequestSchema = new mongoose.Schema(
     },
     {timestamps: true}
 );
-
+connectionRequestSchema.index({fromUserId:1,toUserId:1});
 connectionRequestSchema.pre("save",function(next){
     const connectionRequest = this;
     // Check if from and to userId are equal
